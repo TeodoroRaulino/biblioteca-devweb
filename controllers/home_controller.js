ce364253cc4bc5bb68cdda14f1e0e44dd05924b9
Teodoro <teoirado@gmail.com>
2022-10-27 17:21:13 -0300

    Component Book admin and user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -9,7 +9,69 @@ class HomeController{
   }
 
   async books(req, res){
-    res.render('pages/books', {title: "Livros"})
+
+    const book = [
+      {
+        title: "A volta ao mundo em 80 dias",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Da Terra à Lua",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Viagem ao centro da terra",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "A ilha misteriosa",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Vinte mil léguas submarinas",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "A jangada",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Dois anos de férias",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Cinco semanas em um balão",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Vinte mil léguas submarinas",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Vinte mil léguas submarinas",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      },
+      {
+        title: "Vinte mil léguas submarinas",
+        author: 'Júlio Verne',
+        category: 'SCI-FI'
+      }
+    ]
+
+    res.render('pages/books', {
+      title: "Livros",
+      books: book
+    })
   }
 
 }
