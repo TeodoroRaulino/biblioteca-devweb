0d776658953cbcff6270e4e5099ac077d0ac51d3
Ítalo Viana <italo_1002@live.com>
2022-12-03 18:48:31 -0300

    delete trash

--- services/authentication.js
+++ services/authentication.js
@@ -5,7 +5,6 @@ const jwt = require('jsonwebtoken');
 class Authenticate{
   static login(email, password){
     const user = User.where({email: email})[0]
-    console.log(user)
 
     if(user && user.password === password){
       const token = generate_jwt(user)
@@ -55,9 +54,3 @@ function generate_jwt(user){
 
 
 module.exports = Authenticate
-
-
-Authenticate.logout("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjYsImVtYWlsIjoibWF1cmljaW9AYWx1IiwiaWF0IjoxNjcwMTAzMDgwLCJleHAiOjE2NzAxMDY2ODB9.SpR5ilpqLPY3tDp-M8EXn6jPxRRcjtyyUJWSoCHpr80")
-
-const valid = Authenticate.validate_token("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjYsImVtYWlsIjoibWF1cmljaW9AYWx1IiwiaWF0IjoxNjcwMTAzMDgwLCJleHAiOjE2NzAxMDY2ODB9.SpR5ilpqLPY3tDp-M8EXn6jPxRRcjtyyUJWSoCHpr80")
-console.log(valid)
